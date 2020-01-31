5da1fd5e6bdbc870f0f87aa21d6682730743b0ed
arsts <arstsu@gmail.com>
2020-01-31 17:40:25 +0300

    finished case article carousel and modal

--- src/templates/case-article.js
+++ src/templates/case-article.js
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Carousel from "react-bootstrap/Carousel"
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
+import OverlayTrigger from "react-bootstrap/OverlayTrigger"
+// import Overlay from "react-bootstrap/Overlay"
+import Tooltip from "react-bootstrap/Tooltip"
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 
+import { MdFullscreen } from "react-icons/md"
+
 export default ({ data, pageContext }) => {
   const post = pageContext.allPostData
+
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
   // console.log(JSON.stringify(post, null, 2))
 
   return (
@@ -15,22 +27,60 @@ export default ({ data, pageContext }) => {
       <Header />
       <section className="container">
         <div className="article-container d-xs-flex d-md-block flex-column">
-          <Carousel className="case-carousel mt-2 mr-4 mb-2 float-xs-none float-md-left order-1">
+          <Carousel className="preview-carousel-container mt-2 mr-4 mb-2 float-xs-none float-md-left order-1">
             {post.articleImages.map(image => {
               return (
-                <Carousel.Item>
-                  <img className="" src={image.file.url} alt="" />
+                <Carousel.Item className="preview-carousel-container">
+                  <img src={image.file.url} className="preview-img" alt="" />
                   <Carousel.Caption>
-                    <h3>First slide label</h3>
+                    <h3>
+                      <OverlayTrigger
+                        placement="top"
+                        overlay={<Tooltip>На весь экран</Tooltip>}
+                      >
+                        <MdFullscreen
+                          style={{ cursor: "pointer" }}
+                          onClick={handleShow}
+                        />
+                      </OverlayTrigger>
+                    </h3>
                   </Carousel.Caption>
                 </Carousel.Item>
               )
             })}
           </Carousel>
+
           <h2 className="order-xs-0 order-md-2 order-xs-0">{post.title}</h2>
           <h5>{post.description}</h5>
 
           <article>{documentToReactComponents(post.textBody.json)}</article>
+          {/* Image Modal */}
+          <Modal show={show} size="lg" onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>{post.title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Carousel className="">
+                {post.articleImages.map(image => {
+                  return (
+                    <Carousel.Item>
+                      <img
+                        src={image.file.url}
+                        className="fullscreen-img"
+                        alt=""
+                      />
+                      <Carousel.Caption></Carousel.Caption>
+                    </Carousel.Item>
+                  )
+                })}
+              </Carousel>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Закрыть
+              </Button>
+            </Modal.Footer>
+          </Modal>
 
           {/* <pre>{JSON.stringify(post, null, 4)}</pre> */}
         </div>
