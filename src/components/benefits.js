ad27d46d69903d3e42726b42a37eecd19592a4b1
arsts <arstsu@gmail.com>
2020-02-05 18:21:26 +0300

    replaced benfits svg

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -3,29 +3,42 @@ import React from "react"
 import Cogs from "../images/cogs.png"
 import Form from "../images/form.png"
 import Chart from "../images/chart.png"
+import { FaCogs, FaChartBar, FaFileContract } from "react-icons/fa"
 
 const benefits = () => {
   return (
     <section id="benefits">
       <div className="container d-flex flex-column  flex-lg-row justify-content-lg-between align-items-center">
         <div className="benefit d-flex flex-column  flex-lg-row align-items-center">
-          <img src={Cogs} alt=""></img>
-          <p className="text-center text-lg-left">
-            Многократный опыт внедрения оборудования в существующие технические
-            процессы производств
-          </p>
+          <div>
+            <FaCogs style={{ width: "80px", height: "80px" }} />
+          </div>
+          <div className="ml-xs-1 ml-sm-3 mt-xs-3 mt-md-0">
+            <p className="text-center text-lg-left">
+              Многократный опыт внедрения оборудования в существующие
+              технические процессы производств
+            </p>
+          </div>
         </div>
         <div className="benefit d-flex flex-column  flex-lg-row align-items-center">
-          <img src={Chart} alt=""></img>
-          <p className="text-center text-lg-left">
-            Постоянно растущее число успешно выполненных проектов
-          </p>
+          <div>
+            <FaChartBar style={{ width: "80px", height: "80px" }} />
+          </div>
+          <div className="ml-xs-1 ml-sm-3 mt-xs-3 mt-md-0">
+            <p className="text-center text-lg-left ">
+              Постоянно растущее число успешно выполненных проектов
+            </p>
+          </div>
         </div>
         <div className="benefit d-flex flex-column  flex-lg-row align-items-center">
-          <img src={Form} alt=""></img>
-          <p className="text-center text-lg-left">
-            Подбор оборудования в проектируемых и вновь возводимых объектах
-          </p>
+          <div>
+            <FaFileContract style={{ width: "80px", height: "80px" }} />
+          </div>
+          <div className="ml-xs-0 ml-sm-3 mt-xs-3 mt-md-0">
+            <p className="text-center text-lg-left ">
+              Подбор оборудования в проектируемых и вновь возводимых объектах
+            </p>
+          </div>
         </div>
       </div>
     </section>
--- src/components/questionnarie.js
+++ src/components/questionnarie.js
@@ -36,18 +36,9 @@ function Questionnarie() {
             variant="light"
             className="secondary-button mx-3"
             style={{ width: "170px" }}
-            onClick={handleShow}
           >
             Заполнить
           </Button>
-          <Modal show={show} onHide={handleClose} size="lg">
-            <Modal.Header closeButton>
-              <Modal.Title className="m-3">Опросный лист</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <ClientForm />
-            </Modal.Body>
-          </Modal>
         </div>
       </div>
     </section>
